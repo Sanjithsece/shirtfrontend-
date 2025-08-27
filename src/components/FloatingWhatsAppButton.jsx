419ce35debbc8f115c0020bc380ed1cbc5aa6c9e
Sanjith S <sanjithsece@gmail.com>
2025-08-27 22:24:47 +0530

    changes

--- src/components/FloatingWhatsAppButton.jsx
+++ src/components/FloatingWhatsAppButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const FloatingWhatsAppButton = () => {
-  const whatsappNumber = "919894755053";
+  const whatsappNumber = "+917708440307";
   const message = "Hello! I'm interested in your T-shirts.";
   const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
 
